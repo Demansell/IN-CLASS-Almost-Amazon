65bce41ceeca81145182af03567412c8918a6950
Demansell <demansell2016@gmail.com>
2023-02-21 21:26:32 -0600

    broke the code

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -23,21 +23,21 @@ const navigationEvents = () => {
   document.querySelector('#authors').addEventListener('click', () => {
     console.warn('CLICKED AUTHORS');
   });
-
-  // STRETCH: SEARCH
-  document.querySelector('#search').addEventListener('keyup', (e) => {
-    const searchValue = document.querySelector('#search').value.toLowerCase();
-    console.warn(searchValue);
-
-    // WHEN THE USER PRESSES ENTER, MAKE THE API CALL AND CLEAR THE INPUT
-    if (e.keyCode === 13) {
-      // MAKE A CALL TO THE API TO FILTER ON THE BOOKS
-      // IF THE SEARCH DOESN'T RETURN ANYTHING, SHOW THE EMPTY STORE
-      // OTHERWISE SHOW THE STORE
-
-      document.querySelector('#search').value = '';
-    }
-  });
 };
+
+// STRETCH: SEARCH
+document.querySelector('#search').addEventListener('keyup', (e) => {
+  const searchValue = document.querySelector('#search').value.toLowerCase();
+  console.warn(searchValue);
+
+  // WHEN THE USER PRESSES ENTER, MAKE THE API CALL AND CLEAR THE INPUT
+  if (e.keyCode === 13) {
+    // MAKE A CALL TO THE API TO FILTER ON THE BOOKS
+    // IF THE SEARCH DOESN'T RETURN ANYTHING, SHOW THE EMPTY STORE
+    // OTHERWISE SHOW THE STORE
+
+    document.querySelector('#search').value = '';
+  }
+});
 
 export default navigationEvents;
